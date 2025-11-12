ae7739cc406f3817e9dc812bf36584e5819bc01f
giorgosdata <giorgosigo@icloud.com>
2025-11-12 22:02:34 +0200

    Refactor build script to use Python for voice download
    Removed old installation steps for Piper and Greek voice, added Python script to download voice.

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
-#!/usr/bin/env bash
 set -e
-echo "=== INSTALLING PIPER ==="
-curl -L -o piper_amd64.tar.gz https://github.com/rhasspy/piper/releases/download/2023.11.14/piper_amd64.tar.gz
-tar -xzf piper_amd64.tar.gz
-chmod +x ./piper
-echo "=== INSTALLING GREEK VOICE ==="
-mkdir -p voices
-curl -L -o voice-el-gr-rapunzelina-low.tar.gz https://github.com/rhasspy/piper/releases/download/2023.11.14/voice-el-gr-rapunzelina-low.tar.gz
-tar -xzf voice-el-gr-rapunzelina-low.tar.gz -C voices
-echo "=== BUILD DONE ==="
+
+pip install -r requirements.txt
+
+python - <<'PY'
+from piper.download import download_voice
+download_voice('el-gr-rapunzelina-low', out_dir='voices')
+print("Voice downloaded to ./voices")
+PY
